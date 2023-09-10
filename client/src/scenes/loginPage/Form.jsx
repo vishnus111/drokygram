ec3038caed465369614c2dbf089b00a07de29217
vishnu s <vishnusudeesh390@gmail.com>
2023-09-10 21:25:46 +0530

    form url

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://drokygram.vercel.app/auth/register",
       {
         method: "POST",
         body: formData,
